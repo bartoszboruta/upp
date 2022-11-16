b24d3bd08a5188ab624f39328f49881e0cf4a8e4
bartoszboruta <bboruta.dev@gmail.com>
2022-11-16 18:32:58 +0100

    chore: add description to other typography components

--- src/components/typography/Header/Header.tsx
+++ src/components/typography/Header/Header.tsx
@@ -1,6 +1,13 @@
 import { Text } from "react-native";
 import React from "react";
 
+// - Header
+//     - Should take variant `h1` `h2` `h3` `h4` `h5` props
+//     - Should take `color` prop, default `#070B11`
+//     - Font size `60px` `48px` `44px` `32px` `25px`
+//     - Font weight `700`
+//     - Font family - TBD
+
 type Props = {
   children: React.ReactNode;
 };
--- src/components/typography/Paragraph/Paragraph.tsx
+++ src/components/typography/Paragraph/Paragraph.tsx
@@ -1,6 +1,12 @@
 import { Text } from "react-native";
 import React from "react";
 
+// - Paragraph
+//     - Should take `color` prop, default `#070B11`
+//     - Font size `12px`
+//     - Font weight `600`
+//     - Font family - TBD
+
 type Props = {
   children: React.ReactNode;
 };
