8e08fc0c4354d1258fc99875d001a2d6ef130c67
bartoszboruta <bboruta.dev@gmail.com>
2022-11-16 19:20:55 +0100

    feat: handle inputs, tochables, keyboard

--- App.tsx
+++ App.tsx
@@ -1,35 +1,92 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View } from "react-native";
-
-import Body from "./src/components/typography/Body";
-import Header from "./src/components/typography/Header";
-import Paragraph from "./src/components/typography/Paragraph";
+import React, { useEffect, useRef } from "react";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+  TouchableWithoutFeedback,
+  Keyboard,
+  TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 
 export default function App() {
+  const ref_input2 = useRef<TextInput>(null);
+  const ref_input3 = useRef<TextInput>(null);
+
+  useEffect(() => {
+    ref_input2.current?.focus();
+  }, []);
+
   return (
-    <View
-      style={StyleSheet.flatten([
-        { backgroundColor: "red" },
-        styles.container,
-        styles.container1,
-      ])}
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
     >
-      <Body variant="large">Body</Body>
-      <Header>Header</Header>
-      <Paragraph>Paragraph</Paragraph>
-      <StatusBar style="auto" />
-    </View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.container}
+      >
+        <View style={styles.inner}>
+          <TextInput
+            style={styles.textInput}
+            ref={ref_input2}
+            onSubmitEditing={() => {
+              setTimeout(() => {
+                if (ref_input3.current) {
+                  ref_input3.current.focus();
+                }
+              }, 3000);
+            }}
+            autoCapitalize="words"
+            autoComplete="off"
+            autoCorrect={false}
+            blurOnSubmit={false}
+            returnKeyType="next"
+            placeholder="Email"
+            // keyboardType="email-address"
+          />
+          <TextInput
+            style={styles.textInput}
+            ref={ref_input3}
+            onSubmitEditing={() => {
+              console.log(1234);
+            }}
+            onBlur={() => {
+              console.log("I am blurring");
+            }}
+            returnKeyType="done"
+            placeholder="Password"
+            secureTextEntry={true}
+          />
+          <StatusBar style="auto" />
+          <TouchableOpacity>
+            <Text>Submit</Text>
+          </TouchableOpacity>
+        </View>
+      </KeyboardAvoidingView>
+    </TouchableWithoutFeedback>
   );
 }
 
 const styles = StyleSheet.create({
-  container1: {
-    margin: 20,
-  },
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
+    padding: 16,
+  },
+  inner: {
+    flex: 1,
+    justifyContent: "center",
+  },
+  textInput: {
+    padding: 12,
+    borderRadius: 8,
+    borderColor: "#000ede",
+    borderWidth: 1,
+    marginBottom: 12,
   },
 });
